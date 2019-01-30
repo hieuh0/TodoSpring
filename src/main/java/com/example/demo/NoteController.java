9e7c944b4749aad5f7465dc96eb658b13ad0952e
os-letm <os-letm@OS-LETM.ViettelGroup.ViettelAD.com>
2019-01-30 15:43:59 +0700

    fix

--- src/main/java/com/example/demo/NoteController.java
+++ src/main/java/com/example/demo/NoteController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +26,24 @@ public class NoteController {
 	}
 
 	@PostMapping(path = "/notes")
-	public ResponseEntity<?> addnotes(@RequestBody Notes user) {
-		notesRepository.save(user);
-		return new ResponseEntity<Object>(null, HttpStatus.OK);
+	public Iterable<Notes> addnotes(@RequestBody Notes user) {
+		if(user != null) {
+			notesRepository.save(user);
+		}
+		return notesRepository.findAll();
 	}
 
 	@GetMapping(path = "/notes")
 	public @ResponseBody Iterable<Notes> getAll() {
 		return notesRepository.findAll();
+	}
+	@GetMapping(path="/note/{id}")
+	public Iterable<Notes> deletenote(@PathVariable(value = "ID") Integer id){
+		Optional<Notes> note = notesRepository.findById(id);
+		if(note != null) {
+			System.out.print(note.toString());
+		}
+	//	notesRepository.deleteById(id);
+		return  notesRepository.findAll();
 	}
 }
